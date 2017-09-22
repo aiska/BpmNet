206c2b29fcd7468439e9d3379cafe1e50f0bf1e6
Aiska Hendra <aiskahendra@gmail.com>
2017-09-23 04:13:49 +0700

    add initial WebApi

---
+++ BPMNET.WebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using Swashbuckle.Application;
+using System.Web.Http;
+
+namespace BPMNET.WebApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                 name: "swagger_root",
+                 routeTemplate: "",
+                 defaults: null,
+                 constraints: null,
+                 handler: new RedirectHandler((message => message.RequestUri.ToString()), "swagger"));
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { controller = "swagger", action = "Index", id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ BPMNET.WebApi/Controllers/DeploymentController.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace BPMNET.WebApi.Controllers
+{
+    public class DeploymentController : ApiController
+    {
+        [HttpPost]
+        public async Task<HttpResponseMessage> DeployBpmn()
+        {
+
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            //TODO validate bpmn file
+
+            string root = HttpContext.Current.Server.MapPath("~/App_Data");
+            var provider = new MultipartFormDataStreamProvider(root);
+
+            try
+            {
+                //TODO Parse BPM File
+                // Read the form data.
+                await Request.Content.ReadAsMultipartAsync(provider);
+
+                // This illustrates how to get the file names.
+                foreach (MultipartFileData file in provider.FileData)
+                {
+                    Trace.WriteLine(file.Headers.ContentDisposition.FileName);
+                    Trace.WriteLine("Server file path: " + file.LocalFileName);
+                }
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (System.Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+    }
+}
