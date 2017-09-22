0b93f15b096dd4c6ede9de9e65296c438b3f5cf9
Aiska Hendra <aiskahendra@gmail.com>
2017-09-23 04:25:01 +0700

    add unit test

---
+++ BPMNET.WebApi.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BPMNET.WebApi.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
